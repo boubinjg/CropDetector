b97428c039a162c73c76bdb1ac15f5edeff5a2b9
boubinjg <boubinjg@gmail.com>
2018-11-30 13:44:30 -0500

    Project Done

--- test.py
+++ test.py
@@ -32,7 +32,7 @@ nms = np.zeros([50,75])
 
 thumb = cv2.resize(image, (400,300))
 #orig ranges were 100,200 / 133,266
-'''
+
 for i in range(125,175):
     for j in range(150,225):
         y = i*10
@@ -58,30 +58,10 @@ for i in range(125,175):
             xc = int(x)
             yc = int(y)
             cv2.rectangle(image, (x,y), (x+25, y+25), (255,0,0), -1)
-'''
-print(nms)
+
 np.savetxt('nms', nms)
 
-#orig = 900:2100, 1233:2766
 cv2.imshow("corn",image[1150:1875, 1400:2350])
 cv2.waitKey(0)
 
 
-'''
-# we'll mark our prediction as "correct" of the input image filename
-# contains the predicted label text (obviously this makes the
-# assumption that you have named your testing image files this way)
-filename = args["image"][args["image"].rfind(os.path.sep) + 1:]
-correct = "correct" if filename.rfind(label) != -1 else "incorrect"
-
-# build the label and draw the label on the image
-label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
-output = imutils.resize(output, width=400)
-cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-	0.7, (0, 255, 0), 2)
-
-# show the output image
-print("[INFO] {}".format(label))
-cv2.imshow("Output", output)
-cv2.waitKey(0)
-'''
